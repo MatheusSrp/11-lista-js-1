8a2291386d5f064af7768b4367e5bde80ba5e767
MatheusSrp <matheusvazsrp@gmail.com>
2025-11-14 17:04:48 -0300

    Lista de exercícios By Matheus do Nascimento Gomes Vaz

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 /**** Escreva o código abaixo 👇******/
+const somar = (a, b) => {
+  return (a + b);
+}
+console.log(somar(2, 6));
 
+const multiplicar = (a, b) => {
+  return (a * b);
+}
+console.log(multiplicar(2, 5));
 
+const subtrair = (a, b) => {
+  return (a - b);
+}
+console.log(subtrair(7, 4));
 
+const dividir = (a, b) =>{
+  if(a == 0 || b == 0){
+    return ("Erro: divisão por zero");
+  }
+  else
+    return (a / b);
+}
+console.log(dividir(10, 2)); 
 
+const calcularMedia = (nota1, nota2, nota3, nota4) => {
+  return ((nota1 + nota2 + nota3 + nota4) / 4);
+}
+console.log(calcularMedia(3, 5, 2, 4));
 
+const elevarPotencia = (base, expoente) => {
+  return (Math.pow(base, expoente));
+}
+console.log(elevarPotencia(2, 3));
 
+const calcularAreaRetangulo = (largura, altura) => {
+  return (multiplicar(largura, altura));
+}
+console.log(calcularAreaRetangulo(10, 2));
+
+const verificarParOuImpar = (numero) => {
+  if(numero % 2 === 0) {
+    return ("O número é par");
+  } else{
+    return ("O número é ímpar");
+  }
+}
+console.log(verificarParOuImpar(10));
+console.log(verificarParOuImpar(5));
+
+const calcularRaizQuadrada = (numero) => {
+  return (Math.sqrt(numero));
+}
+console.log(calcularRaizQuadrada(25));
+
+const calcularHipotenusa = (cateto1, cateto2) => {
+  const quadrado1 = elevarPotencia(cateto1, 2);
+  const quadrado2 = elevarPotencia(cateto2, 2);
+  const somar_quadrado = somar(quadrado1, quadrado2);
+  const raiz_quadrado = calcularRaizQuadrada(somar_quadrado);
+  return (raiz_quadrado);
+  
+}
+console.log(calcularHipotenusa(10, 10));
 
 
 /**** Escreva o código acima ☝️******/
